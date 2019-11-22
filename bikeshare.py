9164cdf78765e5c73c84e26622c671061dfc855c
Faisal-Alhazzani <mr.faisal111@gmail.com>
2019-11-22 16:36:19 +0300

    camelCasing get_Filters to getFilter

--- bikeshare.py
+++ bikeshare.py
@@ -6,7 +6,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
-def get_filters():
+def getFilters():
     """
     Asks user to specify a city, month, and day to analyze.
 
@@ -254,7 +254,7 @@ def display_data(df):
 
 def main():
     while True:
-        city, month, day = get_filters()
+        city, month, day = getFilters()
         df = load_data(city, month, day)
 
         time_stats(df)
